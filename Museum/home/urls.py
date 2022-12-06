d011be9ba315bfccf669d2eed39864134a139978
SharvariPatil84 <85826438+SharvariPatil84@users.noreply.github.com>
2022-12-06 00:43:13 -0800

    Add files via upload

---
+++ Museum/home/urls.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.urls import include, path
+from home import views
+
+
+urlpatterns = [
+    path('', views.home, name="home"),
+    path('register', views.register, name="handleregister"),
+    path('log', views.log, name="handlelog"),
+    path('signup', views.handleSign, name="handleSignUp"),
+    path('login', views.handeLogin, name="handleLogin"),
+    path('logout', views.handelLogout, name="handleLogout"),
+    path('contact', views.contact, name="contact"),
+    path('about', views.about, name="about"),
+    # path('/', include('blog.urls'))
+]
